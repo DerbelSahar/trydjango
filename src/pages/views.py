e5b324b6168cf9385162bac46b0b0df26a13abd9
DerbelSahar <derbel.sahar@insat.u-carthage.tn>
2020-06-06 21:36:58 +0100

    code

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,5 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>hello!<h1>")
+def home_view(request, *args, **kwargs):
+    #return HttpResponse("<h1>hello!<h1>")
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    my_context =  {
+        "phone": "+21612312345",
+        "text": "hello",
+        "my_list": [123,1365,561]
+    }
+    return render(request, "about.html", my_context)
+
+def social_view(request, *args, **kwargs):
+   return render(request, "social.html", {})
